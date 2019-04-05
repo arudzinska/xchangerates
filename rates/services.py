d1ddf0cd0dc99db911f4e411b97ebb92c5764c54
arudzinska <ania.ruiva@gmail.com>
2019-04-05 22:48:48 +0200

    added docstrings. Limited currencies list to only today's

--- rates/services.py
+++ rates/services.py
@@ -5,7 +5,14 @@ from datetime import datetime
 from .models import ExchangeRate
 
 
-def get_exchange_rates_and_save(currencies):
+def update_exchange_rates(currencies):
+    """
+    Retrieves newest exchange rates from European Central Bank using an RSS feed reader for the chosen currencies if
+    there is no entry existing yet for today's day in the database.
+
+    :param currencies: iterable
+    :return: None
+    """
 
     for currency in currencies:
         if no_rate_from_today(currency):
@@ -14,8 +21,11 @@ def get_exchange_rates_and_save(currencies):
 
 
 def no_rate_from_today(currency):
+    """
+    Checks if there is already an exchange rate for the given currency and today's day existing in the database.
 
-    if not ExchangeRate.objects.filter(currency=currency, created__date=datetime.now().date()):
-        return True
+    :param currency: <class 'rates.models.Currency'>
+    :return: <class 'bool'>
+    """
 
-    return False
+    return True if not ExchangeRate.objects.filter(currency=currency, created__date=datetime.now().date()) else False
--- rates/views.py
+++ rates/views.py
@@ -1,23 +1,22 @@
-#from django.utils.datastructures import MultiValueDictKeyError
+from datetime import datetime
 
-
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Currency, ExchangeRate
-from .services import get_exchange_rates_and_save
+from .services import update_exchange_rates
 from .serializers import CurrencySerializer, ExchangeRateSerializer
 
 
 class ExchangeRateAPIView(APIView):
     """
-    ?
+    Lists all newest exchange rates (one per each currency).
     """
 
     def get(self, request):
 
-        get_exchange_rates_and_save(currencies=[*Currency.objects.all()])
-        exchangerate_serializer = ExchangeRateSerializer(ExchangeRate.objects.all(), many=True)
+        update_exchange_rates(currencies=Currency.objects.all())
+        exchangerate_serializer = \
+            ExchangeRateSerializer(ExchangeRate.objects.filter(created__date=datetime.now().date()), many=True)
 
         return Response(exchangerate_serializer.data)
